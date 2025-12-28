054de4cf7d00a2bf72a7244c4219d44ad2324434
Mustafa S <mustafasahin@std.iyte.edu.tr>
2025-12-28 13:19:30 +0300

    spring-boot added

---
+++ google-auth-demo/src/test/java/com/example/google_auth_demo/GoogleAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.google_auth_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
